bafd0a1455ff27b0b5996937d8b89f8197ac24fe
JCGViljoen <jcgviljone51@gmail.com>
2023-06-12 16:38:02 +0200

    changes made

--- adminscript/admin.js
+++ adminscript/admin.js
@@ -9,16 +9,17 @@ function onFormSubmit(event) {
   }
   resetForm();
 }
-// retrieve data
+// Retrieve data
 function readFormData() {
   let formData = {};
   formData['productCode'] = document.getElementById('productCode').value;
   formData['product'] = document.getElementById('product').value;
   formData['qty'] = document.getElementById('qty').value;
   formData['price'] = document.getElementById('price').value;
+  formData['image'] = document.getElementById('image').value; // Add the image field
   return formData;
 }
-// insert data
+// Insert data
 function insertNewRecord(data) {
   let table = document.getElementById('storeList').getElementsByTagName('tbody')[0];
   let newRow = table.insertRow(table.length);
@@ -31,36 +32,46 @@ function insertNewRecord(data) {
   let cell4 = newRow.insertCell(3);
   cell4.innerHTML = data.price;
   let cell5 = newRow.insertCell(4);
-  cell5.innerHTML = '<button onclick="editRecord(this)">Edit</button> <button onclick="deleteRecord(this)">Delete</button>';
+  cell5.innerHTML = data.image; // Use the 'image' field value
+  let cell6 = newRow.insertCell(5);
+  cell6.innerHTML = '<button onclick="editRecord(this)">Edit</button> <button onclick="deleteRecord(this)">Delete</button>';
+  // Store the data in local storage
+  let storedData = localStorage.getItem('storedData');
+  let arr = storedData ? JSON.parse(storedData) : [];
+  arr.push(data);
+  localStorage.setItem('storedData', JSON.stringify(arr));
 }
-// reset form fields
+// Reset form fields
 function resetForm() {
   document.getElementById('productCode').value = '';
   document.getElementById('product').value = '';
   document.getElementById('qty').value = '';
   document.getElementById('price').value = '';
+  document.getElementById('image').value = '';
 }
-// edit record
+// Edit record
 function editRecord(button) {
   let row = button.parentNode.parentNode;
   let productCode = row.cells[0].innerHTML;
   let product = row.cells[1].innerHTML;
   let qty = row.cells[2].innerHTML;
   let price = row.cells[3].innerHTML;
+  let image = row.cells[4].innerHTML; // Add the image field
   document.getElementById('productCode').value = productCode;
   document.getElementById('product').value = product;
   document.getElementById('qty').value = qty;
   document.getElementById('price').value = price;
+  document.getElementById('image').value = image;
   selectedRow = row;
 }
-// update record
+// Update record
 function updateRecord(formData) {
   selectedRow.cells[0].innerHTML = formData.productCode;
   selectedRow.cells[1].innerHTML = formData.product;
   selectedRow.cells[2].innerHTML = formData.qty;
   selectedRow.cells[3].innerHTML = formData.price;
 }
-// delete record
+// Delete record
 function deleteRecord(button) {
   let row = button.parentNode.parentNode;
   row.parentNode.removeChild(row);
